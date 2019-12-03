951caf74d22f0007757cf9ec880fb535b807c186
GenTsuki <gentsk77@gmail.com>
2019-12-03 06:30:28 -0500

    attempted changing text colors

--- javafx/src/sample/Main.java
+++ javafx/src/sample/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -21,6 +24,7 @@ public class Main extends Application {
     private Button compileButton;
     private TextArea classPathArea;
     private Button loadClassButton;
+    private TextFlow outputTextFlow;
 
      */
 
@@ -43,6 +47,13 @@ public class Main extends Application {
         //output
         outputTextArea = new TextArea();
         outputTextArea.setEditable(false);
+
+        /*
+        outputTextFlow = new TextFlow();
+        outputTextFlow.maxHeight(500);
+
+         */
+
         prevInput = new ArrayList<>();
 
         // compileButton.setMinWidth(50);
@@ -80,11 +91,33 @@ public class Main extends Application {
                     return;
                 }
 
+                Text textInput = new Text("> " + input + "\n");
+                // outputTextFlow.getChildren().add(textInput);
+
                 outputTextArea.appendText("> " + input + "\n");
                 String output = es.useJshell(input + ";");
                 if (output.length() > 0) {
                     outputTextArea.appendText(output + "\n");
-                    System.out.println("output type is " + es.checkType(input + ";"));
+
+                    Text textOutput = new Text(output + "\n");
+                    // TODO: also change color for error msg
+                    switch (es.checkType(input + ";")) {
+                        case "int":
+                        case "short":
+                        case "long":
+                        case "float":
+                        case "double":
+                            textOutput.setFill(Color.BLUE);
+                            break;
+                        case "String":
+                            textOutput.setFill(Color.RED);
+                            break;
+                        case "char":
+                            textOutput.setFill(Color.DEEPPINK);
+                            break;
+                    }
+                    // System.out.println("output type is " + es.checkType(input + ";"));
+                    // outputTextFlow.getChildren().add(textOutput);
                 }
                 prevInput.add(input);
                 cmdTextArea.clear();
@@ -124,6 +157,7 @@ public class Main extends Application {
 
         // pane.setRight(compileButton);
         pane.setCenter(cmdTextArea);
+        // pane.setBottom(outputTextFlow);
         pane.setBottom(outputTextArea);
         // pane.setLeft(classPathArea);
         // pane.setRight(loadClassButton);
